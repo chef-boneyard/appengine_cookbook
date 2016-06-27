7e2dcb0f5e6536f0d58fc6c700e04f6ee4431057
Nelson Araujo <nelsona@google.com>
2016-06-26 17:34:14 -0700

    Add support to rollover to production & multiple files

--- test/deploy_hello_world.rb
+++ test/deploy_hello_world.rb
@@ -1,13 +1,32 @@
 require 'google/chefconf16'
 
+#BEGIN {
+#
+#  require 'net/http'
+#  
+#  Net::HTTP.module_eval do
+#    alias_method '__initialize__', 'initialize'
+#    
+#    def initialize(*args,&block)
+#      __initialize__(*args, &block)
+#    ensure
+#      @debug_output = $stderr ### if ENV['HTTP_DEBUG']
+#    end
+#  end
+#
+#}
+
 deployer = Google::ChefConf16::AppengineDeploy.new(
-  :app_id => 'google.com:graphite-playground',
+  :app_id => 'graphite-playground-public',
   :service_id => 'default',
-  :bucket_name => 'graphite-playground',
+  :bucket_name => 'graphite-playground-public-store',
   :service_account_json =>
       File.expand_path('~/.config/gcloud/service_account.json'),
   :app_yaml => '../app/app.yaml'
 )
+
+#deployer.test
+#exit
 
 puts "Deploying version #{deployer.version_id}."
 deployer.upload_files
