566f0176cc22fbda30f9fea7491262842b8c464e
Nelson Araujo <nelsona@google.com>
2016-06-26 17:34:14 -0700

    Add support to rollover to production & multiple files

--- libraries/google/chefconf16.rb
+++ libraries/google/chefconf16.rb
@@ -37,45 +37,75 @@ module Google
             if (@service_account_json = args[:service_account_json]).nil? \
                 || @service_account_json.empty?
         raise 'Missing :app_yaml' if (@app_yaml = args[:app_yaml]).nil? || @app_yaml.empty?
+
         @bucket_uri = "#{store_api}/#{@bucket_name}"
         @bucket_path = args[:bucket_path].nil? ? @app_id : args[:bucket_path]
-        @version_id = Time.new.iso8601.to_s.delete('-').delete(':').delete('+').downcase
+
         @version_info = YAML.load(::File.read(@app_yaml))
+
+        @version_id = Time.new.iso8601.to_s.delete('-').delete(':').delete('+').downcase
+        @version_id ||= args[:version_id]
+
+        @cleaned_app_id = @app_id.gsub(/.*:/, '')
+        appengine_domain = @app_id.start_with?('google.com:') ? 'googleplex' : 'appspot'
+        @production_url = "https://#{@cleaned_app_id}.#{appengine_domain}.com/"
+        @staging_url = "https://#{@version_id}-dot-#{@cleaned_app_id}.#{appengine_domain}.com/"
+
         @uploaded_files = {}
-      end
 
-      # Upload our program to a bucket, so deployment can fetch it
-      def upload_files
-        storage = Google::CredentialHelper.new
-                                          .for!(Google::Apis::StorageV1::AUTH_DEVSTORAGE_READ_WRITE)
-                                          .from_service_account_json!(@service_account_json)
-                                          .authorize Google::Apis::StorageV1::StorageService.new
-        Dir.glob("#{::File.dirname(@app_yaml)}/**/*").select do |e|
-          next unless ::File.file? e
-          file_name = ::File.basename(e)
-          file_bucket = "#{@bucket_path}/#{@version_id}/#{file_name}"
-          print "Uploading file to gs://#{@bucket_name}/#{file_bucket}..."
-          storage.insert_object(
-            @bucket_name,
-            Google::Apis::StorageV1::Object.new(
-              :id => file_bucket
-            ),
-            :name => file_bucket,
-            :upload_source => ::File.open(e)
-          )
-          @uploaded_files[file_name] = file_bucket if file_name != 'app.yaml'
-          puts ' done.'
-        end
-      end
-
-      # Create a new version of the application
-      def create_new_version
         @app_engine =
           Google::CredentialHelper.new
                                   .for!(Google::Apis::AppengineV1beta5::AUTH_CLOUD_PLATFORM)
                                   .from_service_account_json!(@service_account_json)
                                   .authorize Google::Apis::AppengineV1beta5::AppengineService.new
+      end
 
+      # Upload our program to a bucket, so deployment can fetch it
+      #
+      # Note: this version does not support mapping using regular expressions.
+      def upload_files
+        storage = Google::CredentialHelper.new
+                                          .for!(Google::Apis::StorageV1::AUTH_DEVSTORAGE_READ_WRITE)
+                                          .from_service_account_json!(@service_account_json)
+                                          .authorize Google::Apis::StorageV1::StorageService.new
+        app_yaml_dir = ::File.expand_path(::File.dirname(@app_yaml))
+
+        @version_info['handlers'].each do |handler|
+          if handler.key?('static_files')
+            file_glob = "#{app_yaml_dir}/#{handler['upload']}"
+          elsif handler.key?('static_dir')
+            # TODO(nelsona): Add support to directories
+            raise 'Uploading directories is not supported'
+          elsif handler.key?('script')
+            script = handler['script']
+            file_glob = "#{app_yaml_dir}/#{::File.basename(script, ::File.extname(script))}.*"
+          else
+            raise 'Unknown handler type'
+          end
+
+          Dir.glob(file_glob).select do |e|
+            file_name = Pathname.new(e).relative_path_from Pathname.new(app_yaml_dir)
+            file_bucket = "#{@bucket_path}/#{@version_id}/#{file_name}"
+            STDERR.print "Uploading file to gs://#{@bucket_name}/#{file_bucket}..."
+            storage.insert_object(
+              @bucket_name,
+              Google::Apis::StorageV1::Object.new(
+                :id => file_bucket
+              ),
+              :name => file_bucket,
+              :upload_source => ::File.open(e)
+            )
+            @uploaded_files[handler] = {
+              :filename => file_name,
+              :bucket => file_bucket
+            }
+            STDERR.puts ' done.'
+          end
+        end
+      end
+
+      # Create a new version of the application
+      def create_new_version
         version = Google::Apis::AppengineV1beta5::Version.new(
           :id => @version_id,
           :name => "apps/#{@app_id}/services/#{@service_id}/versions/#{@version_id}",
@@ -83,42 +113,64 @@ module Google
           :runtime => @version_info['runtime'],
           :threadsafe => @version_info['threadsafe'],
           :handlers => @version_info['handlers'].map do |handler|
-            Google::Apis::AppengineV1beta5::UrlMap.new(
-              :url_regex => handler['url'],
-              :script => Google::Apis::AppengineV1beta5::ScriptHandler.new(
-                :script_path => handler['script']))
-          end,
+            if handler.key?('static_files')
+              Google::Apis::AppengineV1beta5::UrlMap.new(
+                :url_regex => handler['url'],
+                :static_files => Google::Apis::AppengineV1beta5::StaticFilesHandler.new(
+                  :path => handler['static_files'],
+                  :upload_path_regex => "#{@bucket_uri}/#{@uploaded_files[handler][:bucket]}"))
+            elsif handler.key?('static_dir')
+              # TODO(nelsona): Add support to directories
+              raise 'Uploading directories is not supported'
+            elsif handler.key?('script')
+              Google::Apis::AppengineV1beta5::UrlMap.new(
+                :url_regex => handler['url'],
+                :script => Google::Apis::AppengineV1beta5::ScriptHandler.new(
+                  :script_path => handler['script']))
+            else
+              raise 'Unknown handler tye'
+            end
+          end
+          .compact,
           :deployment => {
-            :files => Hash[*@uploaded_files.map do |name, path|
-              [name, Google::Apis::AppengineV1beta5::FileInfo.new(
-                :source_url => "#{@bucket_uri}/#{path}")]
+            :files => Hash[*@uploaded_files.map do |_, path|
+              [::File.basename(path[:filename]), Google::Apis::AppengineV1beta5::FileInfo.new(
+                :source_url => "#{@bucket_uri}/#{path[:bucket]}")]
             end
             .flatten]
           }
         )
 
-        print 'Requesting create new version for the application...'
+        STDERR.print 'Requesting create new version for the application...'
         new_version = @app_engine.create_app_service_version(
           @app_id, @service_id, version)
-        puts ' done.'
+        STDERR.puts ' done.'
 
         @operation_id = new_version.name.split('/').last
       end
 
       # Wait for an operation to complete
       def wait_until_complete
-        print 'Waiting for deployment to complete...'
-        until @app_engine.get_app_operation(@app_id, @operation_id).done?
-          print '.'
+        STDERR.print 'Waiting for deployment to complete...'
+        until (operation = @app_engine.get_app_operation(@app_id, @operation_id)).done?
+          STDERR.print '.'
           sleep 1
         end
-        puts ' done.'
-        @staging_url = "https://#{@version_id}-dot-#{@app_id.gsub(/.*:/, '')}.googleplex.com/"
+        STDERR.puts ' done.'
+        raise "Failed to deploy application: #{operation.error.inspect}" unless operation.error.nil?
       end
 
       # Activate App Engine application
+      #
+      # Note: In a real production application you may want a smooth traffic transition. This
+      # application does a one shot transition only.
       def activate
-        @production_url = "https://#{@app_id.gsub(/.*:/, '')}.googleplex.com/"
+        STDERR.print "Activating version #{@version_id}..."
+        service = @app_engine.get_app_service(@app_id, @service_id)
+        service.split.allocations.clear
+        service.split.allocations[@version_id] = 1
+        @app_engine.patch_app_service(@app_id, @service_id, service, :mask => 'split')
+        STDERR.puts ' done.'
       end
 
       def cleanup
